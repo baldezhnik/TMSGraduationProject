a329634c160a52f1102e192a4f7679c5a5e34d77
baldezhnik <drako.dima@gmail.com>
2022-09-04 01:16:32 +0300

    create notification

--- app/src/main/java/com/dsd/tmsgraduationproject/MainActivity.kt
+++ app/src/main/java/com/dsd/tmsgraduationproject/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.dsd.tmsgraduationproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.dsd.tmsgraduationproject.databinding.ActivityMainBinding
-import com.dsd.tmsgraduationproject.room.Repositories
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private companion object{
+        private const val CHANNEL_ID = "channel1"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,5 +39,48 @@ class MainActivity : AppCompatActivity() {
         ))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navBottom.setupWithNavController(navController)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        showNotification()
+    }
+
+    private fun showNotification(){
+        createNotificationChannel()
+        val date = Date()
+        val notificationId = SimpleDateFormat("ddHHmmss", Locale.US).format(date).toInt()
+
+        val resultIntent = Intent(this, MainActivity::class.java)
+        val resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent,
+            PendingIntent.FLAG_IMMUTABLE)
+
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(getString(R.string.notification_title))
+            .setContentText(getString(R.string.notification_text))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(resultPendingIntent)
+            .setAutoCancel(true)
+            .setShowWhen(true)
+
+        val notificationManagerCompat = NotificationManagerCompat.from(this)
+        notificationManagerCompat.notify(notificationId, notificationBuilder.build())
+
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = (R.string.channel_name).toString()
+            val descriptionText = (R.string.channel_description).toString()
+
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
